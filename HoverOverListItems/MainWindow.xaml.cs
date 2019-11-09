11879a04034e48b2631382f96b088fdf4901a52b
Joshua <joshua.richardson@myunidays.com>
2019-11-09 08:16:35 +0000

    Added new project to change hover styles

---
+++ HoverOverListItems/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace HoverOverListItems
+{
+    public class HoverOverItemsInList<T> : ListBox
+        where T : class
+    {
+        public HoverOverItemsInList()
+        {
+
+        }
+    }
+    public class HoverList : HoverOverItemsInList<Item> { }
+
+    public class Item
+    {
+        public string Name { get; set; }
+    }
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public IList<Item> items = new ObservableCollection<Item>();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            items.Add(new Item { Name = "1" });
+            items.Add(new Item { Name = "2" });
+            items.Add(new Item { Name = "3" });
+
+            listBox.DataContext = items;
+        }
+    }
+}
