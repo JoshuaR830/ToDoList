71b2fd9726fa500d156c8408a4bf3720fd9911dd
Joshua <psyjr4@nottingham.ac.uk>
2019-11-10 11:30:47 +0000

    Event listener stuff

--- HoverOverListItems/MainWindow.xaml.cs
+++ HoverOverListItems/MainWindow.xaml.cs
@@ -18,6 +18,7 @@ namespace HoverOverListItems
     public class Item
     {
         public string Name { get; set; }
+		public int Id { get; set; }
     }
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -30,11 +31,53 @@ namespace HoverOverListItems
         {
             InitializeComponent();
 
-            items.Add(new Item { Name = "1" });
-            items.Add(new Item { Name = "2" });
-            items.Add(new Item { Name = "3" });
+            items.Add(new Item { Name = "1", Id = 0 });
+            items.Add(new Item { Name = "2", Id = 1 });
+            items.Add(new Item { Name = "3", Id = 2 });
 
             listBox.DataContext = items;
         }
-    }
+
+		//private void MouseDownButton(object sender, System.Windows.Input.MouseEventArgs e)
+		//{
+		//	items.Add(new Item { Name = "4" });
+		//	listBox.DataContext = items;
+		//}
+
+		private void MouseOverButton(object sender, System.Windows.Input.MouseEventArgs e)
+		{
+			Button1.Content = "Hover";
+		}
+
+		private void MouseDownButton(object sender, System.Windows.Input.MouseButtonEventArgs e)
+		{
+			
+		}
+
+		private void MouseClicked(object sender, RoutedEventArgs e)
+		{
+			items.Add(new Item { Name = "Left Click" });
+			listBox.DataContext = items;
+		}
+
+		private void MiddleClick(object sender, System.Windows.Input.MouseWheelEventArgs e)
+		{
+			items.Add(new Item { Name = "Middle Click" });
+			listBox.DataContext = items;
+		}
+
+		private void TextClicked(object sender, System.Windows.Input.MouseButtonEventArgs e)
+		{
+			items[1].Name = "Clicked text";
+			listBox.DataContext = items;
+		}
+
+		private void ItemChanged(object sender, SelectionChangedEventArgs e)
+		{
+			var item = listBox.SelectedItem as Item;
+			items[item.Id].Name = "Renamed";
+			listBox.DataContext = items;
+
+		}
+	}
 }
